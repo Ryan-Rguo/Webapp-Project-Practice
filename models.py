a8c2ebd2a23eea8e7fd2ee1caee467d5c73f8392
Ryan-Rguo <dulandal@hotmail.com>
2020-10-15 15:28:10 +1100

    update models.py

--- models.py
+++ models.py
@@ -15,7 +15,7 @@ def next_id():
     return '%015d%s000' % (int(time.time() * 1000), uuid.uuid4().hex)
 
 class User(Model):
-    __table__ = 'user'
+    __table__ = 'users'
 
     id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
     email = StringField(ddl='varchar(50)')
@@ -40,7 +40,7 @@ class Blog(Model):
 class Comment(Model):
     __table__ = 'comments'
 
-    id = StringField(ddl='varchar(50)')
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
     blog_id = StringField(ddl='varchar(50)')
     user_id = StringField(ddl='varchar(50)')
     user_name = StringField(ddl='varchar(50)')
