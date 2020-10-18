6336a7ab0473e10d870eaa1f1b854b3dc16d843f
Ryan-Rguo <dulandal@hotmail.com>
2020-10-18 11:56:12 +1100

    create blog page & update static files

--- app.py
+++ app.py
@@ -101,11 +101,11 @@ def datetime_filter(t):
     if delta < 60:
         return u'1 min ago'
     if delta < 3600:
-        return u'%s mins ago' % (delta // 60)
+        return u'%s min(s) ago' % (delta // 60)
     if delta < 86400:
-        return u'%s hours ago' % (delta // 3600)
+        return u'%s hour(s) ago' % (delta // 3600)
     if delta < 604800:
-        return u'%s days ago' % (delta // 86400)
+        return u'%s day(s) ago' % (delta // 86400)
     dt = datetime.fromtimestamp(t)
     return u'%s-%s-%s' % (dt.year, dt.month, dt.day)
 
